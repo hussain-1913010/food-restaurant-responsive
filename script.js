053063303185f31ea80580cc76dce54f9f3a2eb4
hussain-1913010 <hussain1913010@stud.kuet.ac.bd>
2021-09-15 18:12:41 +0600

    slider done

--- script.js
+++ script.js
@@ -27,3 +27,20 @@ document.querySelector('#close').onclick = () => {
 
 
 
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 3500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    // navigation: {
+    //   nextEl: ".swiper-button-next",
+    //   prevEl: ".swiper-button-prev",
+    // },
+    loop:true,
+  });
