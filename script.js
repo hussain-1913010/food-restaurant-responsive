ddc1a99d5e0beee60327afab53a0979eece6fe9f
hussain-1913010 <hussain1913010@stud.kuet.ac.bd>
2021-10-18 20:31:59 +0600

    review sectin done

--- script.js
+++ script.js
@@ -44,3 +44,43 @@ var swiper = new Swiper(".mySwiper", {
     // },
     loop:true,
   });
+
+
+
+  var swiper = new Swiper(".reviewSlider", {
+    spaceBetween: 20,
+    centeredSlides: true,
+    autoplay: {
+      delay: 7500,
+      disableOnInteraction: false,
+    },
+    // pagination: {
+    //   el: ".swiper-pagination",
+    //   clickable: true,
+    // },
+    // navigation: {
+    //   nextEl: ".swiper-button-next",
+    //   prevEl: ".swiper-button-prev",
+    // },
+    loop:true,
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      640: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    }
+  });
+
+
+
+
+
+
